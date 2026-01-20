604a075f1c193d76c8afbbf578249648fb0a97eb
Omchavan02 <surendrachavan367@gmail.com>
2026-01-20 22:44:37 +0530

    Fix case sensitivity in activityRoutes.js for Linux deployment

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -4,7 +4,7 @@ import {
   getActivities,
   updateActivity,
   deleteActivity
-} from "../controllers/activitycontroller.js";
+} from "../controllers/activityController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
